75d12d7a40b2adaad4fd0ea2c21aaf17df76a5bc
tunca.tunc <tunca.tunc@pro-line.com.tr>
2017-01-11 18:05:50 +0300

    Video List

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -2,15 +2,17 @@ import React from 'react';
 import VideoListItem from './video-list-item'
 
 const VideoList = (props) => {
-
-    return (
-        <ul className="col-md-4 list-group">
-        props.videos.map((video) => {
-            return (
-                <VideoListItem video={video}/>
-            );
-        })
-        </ul>
+  const videoList = 
+    props.videos.map((video) => {
+        return (
+            <VideoListItem video={video}/>
+        );
+    });
+    
+    return(
+      <ul className="col-md-4 list-group">
+        {videoList}
+      </ul>
     );
 };
 
